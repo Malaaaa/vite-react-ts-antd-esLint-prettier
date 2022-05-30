86d216aa8d3ca1e1e54c52abdd0a3795329e4f84
Malaaaa <37627837+Malaaaa@users.noreply.github.com>
2022-05-30 14:05:06 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { Button } from 'antd';
+import './App.css';
+import './App.less';
+
+const App: FC = () => (
+  <div className="App centerContainer">
+    <Button type="primary">Welcome to React-TS-Antd</Button>
+  </div>
+);
+
+export default App;
